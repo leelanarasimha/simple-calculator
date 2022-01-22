f1ee1802102cff7fa059147954105e6a21c210eb
leelanarasimha <leela.narsimha@gmail.com>
2022-01-22 17:11:27 +0530

    calculate function generate

---
+++ main.js
@@ -0,0 +1,9 @@
+function calculate(event) {
+  const inputValue = event.target.value;
+  const expression = /\+|\*|\-|\//;
+  
+  const numberA;
+  const numberB;
+  const operator;
+}
+document.getElementById('inputValue').addEventListener('change', calculate);
