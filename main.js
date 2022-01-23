adb25f456947fd4dcf985b96eb7490b49fec54a5
leelanarasimha <leela.narsimha@gmail.com>
2022-01-23 19:47:36 +0530

    calculato 4 video

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 function calculate(event) {
   const inputValue = event.target.value;
   const expression = /\+|\*|\-|\//;
-  
-  const numberA;
-  const numberB;
-  const operator;
+
+  const numbers = inputValue.split(expression);
+  const numberA = +numbers[0];
+  const numberB = +numbers[1];
+
+  const operation = inputValue.match(expression);
+  const operator = operation[0];
+
+  const calculator = new Calculator();
+  calculator.add(numberA);
+  calculator.add(numberB);
+  console.log(calculator.total);
 }
+
 document.getElementById('inputValue').addEventListener('change', calculate);
