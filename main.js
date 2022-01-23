288e677e61365f7b6393536f600574c9e91dc326
leelanarasimha <leela.narsimha@gmail.com>
2022-01-23 20:15:46 +0530

    5 video switch case

--- main.js
+++ main.js
@@ -11,8 +11,30 @@ function calculate(event) {
 
   const calculator = new Calculator();
   calculator.add(numberA);
-  calculator.add(numberB);
-  console.log(calculator.total);
+
+  let result;
+  switch (operator) {
+    case '+':
+      result = calculator.add(numberB);
+      break;
+    case '-':
+      result = calculator.subtract(numberB);
+      break;
+    case '*':
+      result = calculator.multiply(numberB);
+      break;
+    case '/':
+      result = calculator.divide(numberB);
+      break;
+  }
+  updateResult(result);
+}
+
+function updateResult(result) {
+  let element = document.getElementById('result');
+  if (element) {
+    element.innerText = result;
+  }
 }
 
 document.getElementById('inputValue').addEventListener('change', calculate);
