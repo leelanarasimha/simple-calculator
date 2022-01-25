99650c31dd476c82b3d75dcd6aa69180ff74f48c
leelanarasimha <leela.narsimha@gmail.com>
2022-01-25 23:07:21 +0530

    first spec file

--- calculator.spec.js
+++ calculator.spec.js
@@ -1,4 +1,17 @@
 describe('Calculator.js', function () {
-  //ToDO: tests or specs
-  //test case od specs
+  it('should add number to the total', function () {
+    //TODO: Expectations
+  });
+
+  it('should subtract number from the total', function () {
+    //TODO: Expectations
+  });
+
+  it('should multiply number with the total', function () {
+    //TODO: Expectations
+  });
+
+  it('should divide number by the total', function () {
+    //TODO: Expectations
+  });
 });
