e94348f4328225d7d170e93e165511638fa73b90
leelanarasimha <leela.narsimha@gmail.com>
2022-01-26 23:23:06 +0530

    calculator expectations

--- calculator.spec.js
+++ calculator.spec.js
@@ -1,6 +1,12 @@
 describe('Calculator.js', function () {
   it('should add number to the total', function () {
-    //TODO: Expectations
+    //expect 5+5 to be 10
+    //expect(5 + 5).toBe(10);
+
+    const calculator = new Calculator();
+    calculator.add(5);
+    //expect total to be 5
+    expect(calculator.total).toBe(5);
   });
 
   it('should subtract number from the total', function () {
