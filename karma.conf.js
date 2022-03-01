4248946d2524c5b8e4d94b4b606cedf7bbae7513
leelanarasimha <leela.narsimha@gmail.com>
2022-03-02 00:47:32 +0530

    coverage reports

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,18 @@
 module.exports = function (config) {
   config.set({
     frameworks: ['jasmine'],
+    preprocessors: {
+      '*.js': 'coverage'
+    },
     files: ['./CustomMatcher.js', '*.js', '*.spec.js'],
-    plugins: ['karma-jasmine', 'karma-chrome-launcher'],
-    reporters: ['dots'],
+    plugins: ['karma-jasmine', 'karma-chrome-launcher', 'karma-coverage'],
+    reporters: ['dots', 'coverage'],
     colors: true,
     singleRun: true,
-    browsers: ['ChromeHeadless']
+    browsers: ['ChromeHeadless'],
+    coverageReporter: {
+      type: 'html',
+      dir: 'coverage/'
+    }
   });
 };
